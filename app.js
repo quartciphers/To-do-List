c9ee4739788b1f5b1cbd889fb317c1548fa806c2
Ray <kathirkarthik2602@gail.com>
2021-02-23 11:51:16 +0530

    alpha-2

--- app.js
+++ app.js
@@ -161,8 +161,14 @@ app.post("/delete",function(req,res){
   
 })
 
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port=3000;
 
-app.listen(3000,function(){
+}
+
+
+app.listen(port,function(){
 
     console.log("Server listen on 3000");
 
